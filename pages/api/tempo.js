0eec685d4dfc8ea011eb88f8f5b72a8f6e211181
zeroffbr <wennerjohnsilva01@gmail.com>
2022-06-01 13:56:06 -0300

    roda api/tempo cep

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,15 +1,17 @@
 export default async function tempo(request, response){
-    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+    // const apiSecret = process.env.CONVERTKIT_API_SECRET;
+    const cep = '62870000';
     const dynamicDate = new Date();
-
-    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
-    const subscribersResponseJson = await subscribersResponse.json();
-    const inscritos = subscribersResponseJson.total_subscribers;
+//ZJwLOhi01EvB90CIpsyw1e0fGRzPfw1Q
+    const subscribersResponse = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
+    const cepResponseJson = await subscribersResponse.json();
+    const city = cepResponseJson.city;
 
     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
     response.json({
         date: dynamicDate.toGMTString(),
-        inscritos: inscritos
+        cep: cepResponseJson,
+        city: city
     })
 }
