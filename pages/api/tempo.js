401a823c6bb2606a045b5f513e97d77c0b1bd665
zeroffbr <wennerjohnsilva01@gmail.com>
2022-06-01 13:22:11 -0300

    roda api/tempo

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,15 @@
 export default function tempo(request, response){
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
+
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson.total_subscribers;
+
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
     response.json({
-        date:dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
     })
 }
